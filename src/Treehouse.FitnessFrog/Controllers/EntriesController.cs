d7e1b7fb5f0d52d4fd42f5e3c5e02aac0f707d31
SangVieHuy <hvsang2101@gmail.com>
2018-05-21 15:04:25 +0700

    Added EntriesController Add POST action method.

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -38,8 +38,12 @@ namespace Treehouse.FitnessFrog.Controllers
 
             return View(entries);
         }
-
         public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost, ActionName("Add")]
+        public ActionResult AddPost()
         {
             return View();
         }
